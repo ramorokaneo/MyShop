de55d72457ee0a7e2c9252ddd36d186b2eb76944
Neo <ramoroka.neo@gmail.com>
2023-07-20 17:50:44 +0200

    main

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,33 +1,35 @@
 import React from 'react';
-import {View, SafeAreaView, Image, Text, StyleSheet} from 'react-native';
+import { View, SafeAreaView, Image, Text, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import COLORS from '../consts/colors';
 
-const DetailsScreen = ({navigation, route}) => {
-  const plant = route.params;
+const DetailsScreen = ({ navigation, route }) => {
+  const fruit = route.params;
 
   return (
     <SafeAreaView
       style={{
         flex: 1,
         backgroundColor: COLORS.white,
-      }}>
-      <View style={style.header}>
+      }}
+    >
+      <View style={styles.header}>
         <Icon name="arrow-back" size={28} onPress={() => navigation.goBack()} />
         <Icon name="shopping-cart" size={28} />
       </View>
-      <View style={style.imageContainer}>
-        <Image source={fruit.img} style={{resizeMode: 'contain', flex: 1}} />
+      <View style={styles.imageContainer}>
+        <Image source={fruit.img} style={{ resizeMode: 'contain', flex: 1 }} />
       </View>
-      <View style={style.detailsContainer}>
+      <View style={styles.detailsContainer}>
         <View
           style={{
             marginLeft: 20,
             flexDirection: 'row',
             alignItems: 'flex-end',
-          }}>
-          <View style={style.line} />
-          <Text style={{fontSize: 18, fontWeight: 'bold'}}>Best choice</Text>
+          }}
+        >
+          <View style={styles.line} />
+          <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Best choice</Text>
         </View>
         <View
           style={{
@@ -36,61 +38,68 @@ const DetailsScreen = ({navigation, route}) => {
             flexDirection: 'row',
             justifyContent: 'space-between',
             alignItems: 'center',
-          }}>
-          <Text style={{fontSize: 22, fontWeight: 'bold'}}>{fruit.name}</Text>
-          <View style={style.priceTag}>
+          }}
+        >
+          <Text style={{ fontSize: 22, fontWeight: 'bold' }}>{fruit.name}</Text>
+          <View style={styles.priceTag}>
             <Text
               style={{
                 marginLeft: 15,
                 color: COLORS.white,
                 fontWeight: 'bold',
                 fontSize: 16,
-              }}>
-              ${plant.price}
+              }}
+            >
+              ${fruit.price}
             </Text>
           </View>
         </View>
-        <View style={{paddingHorizontal: 20, marginTop: 10}}>
-          <Text style={{fontSize: 20, fontWeight: 'bold'}}>About</Text>
+        <View style={{ paddingHorizontal: 20, marginTop: 10 }}>
+          <Text style={{ fontSize: 20, fontWeight: 'bold' }}>About</Text>
           <Text
             style={{
               color: 'grey',
               fontSize: 16,
               lineHeight: 22,
               marginTop: 10,
-            }}>
-            {plant.about}
+            }}
+          >
+            {fruit.about}
           </Text>
           <View
             style={{
               marginTop: 20,
               flexDirection: 'row',
               justifyContent: 'space-between',
-            }}>
+            }}
+          >
             <View
               style={{
                 flexDirection: 'row',
                 alignItems: 'center',
-              }}>
-              <View style={style.borderBtn}>
-                <Text style={style.borderBtnText}>-</Text>
+              }}
+            >
+              <View style={styles.borderBtn}>
+                <Text style={styles.borderBtnText}>-</Text>
               </View>
               <Text
                 style={{
                   fontSize: 20,
                   marginHorizontal: 10,
                   fontWeight: 'bold',
-                }}>
+                }}
+              >
                 1
               </Text>
-              <View style={style.borderBtn}>
-                <Text style={style.borderBtnText}>+</Text>
+              <View style={styles.borderBtn}>
+                <Text style={styles.borderBtnText}>+</Text>
               </View>
             </View>
 
-            <View style={style.buyBtn}>
+            <View style={styles.buyBtn}>
               <Text
-                style={{color: COLORS.white, fontSize: 18, fontWeight: 'bold'}}>
+                style={{ color: COLORS.white, fontSize: 18, fontWeight: 'bold' }}
+              >
                 Buy
               </Text>
             </View>
@@ -101,7 +110,7 @@ const DetailsScreen = ({navigation, route}) => {
   );
 };
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   header: {
     paddingHorizontal: 20,
     marginTop: 20,
@@ -139,7 +148,7 @@ const style = StyleSheet.create({
     width: 60,
     height: 40,
   },
-  borderBtnText: {fontWeight: 'bold', fontSize: 28},
+  borderBtnText: { fontWeight: 'bold', fontSize: 28 },
   buyBtn: {
     width: 130,
     height: 50,
